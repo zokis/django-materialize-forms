38fb1e5803e3c0cc65f61f661012dab33a1f4ea9
Marcelo Tambalo <marcelo.zokis@gmail.com>
2015-06-09 16:20:06 -0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='django-materialize-forms',
+    version='0.1.0',
+    url='https://github.com/zokis/django-materialize-forms',
+    author='Marcelo Fonseca Tambalo',
+    author_email='marcelo.zokis@gmail.com',
+    license='MIT',
+    packages=['materialize_forms', 'materialize_forms.templatetags'],
+    include_package_data=True,
+    description='Materializecss support for Django projects',
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Utilities",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+    ],
+)

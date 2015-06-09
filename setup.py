6e770b00e1f59d1fcabc7eabc3519723818f757f
Marcelo Tambalo <marcelo.zokis@gmail.com>
2015-06-09 16:38:26 -0300

    update setup and add static

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='django-materialize-forms',
-    version='0.1.0',
+    version='0.1.2',
     url='https://github.com/zokis/django-materialize-forms',
     author='Marcelo Fonseca Tambalo',
     author_email='marcelo.zokis@gmail.com',
